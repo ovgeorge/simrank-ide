da33db6a262c5209bf4a6bc0e2357b5f20b8e3c3
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2015-05-06 20:25:54 +0300

    Fix compactify script

--- simplewiki/compactify.py
+++ simplewiki/compactify.py
@@ -2,37 +2,36 @@ from cPickle import load, dump
 from scipy.io import savemat, mmwrite
 from scipy.sparse import coo_matrix
 
-path_to_data = ''
-
-
 def create_compact_dicts():
     print 'Compactifying...'
     sparse_to_dense, dense_to_sparse = {}, {}
-    k = 0
-    for line in open(path_to_data + 'graph.txt'):
+    k = 0.0
+    f = open('graph.txt')
+    lines = f.readlines()
+    f.close()
+    mk_step = 20
+    step = len(lines) / mk_step
+    for l in lines:
         k = k + 1
-        if k % 100000 == 0:
-            print k
-        # FIX id generation!!!1111
-        IDs = line.split()
+        if k % step == 0:
+            print "%d%% done" % (100 / mk_step * k / step)
+        IDs = l.split()
         for id in IDs:
             ID = int(id)
             if ID not in sparse_to_dense:
                 i = len(dense_to_sparse)
                 sparse_to_dense[ID] = i
                 dense_to_sparse[i] = ID
-    print len(sparse_to_dense)
-    print len(dense_to_sparse)
-    dump(sparse_to_dense, open(path_to_data + 'sparse_to_dense.pickle', 'w'), 2)
-    dump(dense_to_sparse, open(path_to_data + 'dense_to_sparse.pickle', 'w'), 2)
+    dump(sparse_to_dense, open('sparse_to_dense.pickle', 'w'), 2)
+    dump(dense_to_sparse, open('dense_to_sparse.pickle', 'w'), 2)
     print 'Compactifying... Done'
 
 
 def create_matrix():
-    sparse_to_dense = load(open(path_to_data + 'sparse_to_dense.pickle'))
+    sparse_to_dense = load(open('sparse_to_dense.pickle'))
     print 'Reading graph file and matrixifying...'
     I, J = [], []
-    for line in open(path_to_data + 'graph.txt'):
+    for line in open('graph.txt'):
         converted = [sparse_to_dense.get(int(ID)) for ID in line.split()]
         i = converted[0]
         j = converted[1]
@@ -47,7 +46,7 @@ def create_matrix():
 def main():
     create_compact_dicts()
     W = create_matrix()
-    savemat(path_to_data + 'W.mat', dict(W = W + W.T))
+    savemat('W.mat', dict(W = W + W.T))
 
 if __name__ == '__main__':
     main()
